9781de6e2c4dd263b7ae7a0dbf20fe137c276c8b
Leehyunjeong <68942045+Hyun2gi@users.noreply.github.com>
2024-01-27 11:04:00 +0900

    깃 오류 없애기 위해 커밋

--- Client/Header/DynamicCamera.h
+++ Client/Header/DynamicCamera.h
@@ -44,28 +44,18 @@ private:
 	_bool		m_bCheck = false;
 	_bool		m_bChaseInit = true;
 
-	// X,Y,Z의 회전각
-	float		m_fAngleX;
-	float		m_fAngleY;
-	float		m_fAngleZ;
-
-	// 회전스피드
-	float		m_fAngleSpeed;
-	// 이동 스피드
-	float		m_fMoveSpeed;
-
-	// 플레이어와 카메라의 사이 거리와 방향
-	// 플레이어에서 해당방향 더해주기
+	//플레이어와 카메라의 사이 거리와 방향
+	//플레이어에서 해당방향 더해주기
 	_vec3		m_vCameraPosDir;
-	// 눈과 바라보는 지점의 고정거리
-	// player를 따라다닐때 초기화할때의 값
+
+	//x,z로의 길이
 	float		m_fCameraDistance;
-	// y로의 길이 (플레이어 추적할때 초기화에 사용)
+
+	//y로의 길이
 	float		m_fCameraHeight;
 
 	CTransform* m_pTarget;
 
-	// 카메라 사용용도
 	DYNAMICCAMERAID		m_eCurState;
 
 	//필요없을시 삭제
