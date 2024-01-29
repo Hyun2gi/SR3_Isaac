873ff517ada693eae3e80ba6a08ee9e2aaa3f6c1
YEHYeWon <116511436+YEHYeWon@users.noreply.github.com>
2024-01-29 18:59:59 +0900

    [혜원] 01.29 19:00 클라이언트 로드해서 파싱 완료 / 오브젝트와 몬스터 수정 후 마저 작업 필요

--- Client/Header/LoadStage.h
+++ Client/Header/LoadStage.h
@@ -2,8 +2,16 @@
 
 #include "Scene.h"
 
+#include "Loading.h"
+
 class CLoadStage : public Engine::CScene
 {
+private:
+	struct LoadObj
+	{
+		int iType, iIndex, iX, iY, iZ;
+	};
+
 private:
 	explicit CLoadStage(LPDIRECT3DDEVICE9 pGraphicDev);
 	virtual ~CLoadStage();
@@ -15,11 +23,17 @@ public:
 	virtual void Render_Scene() override;
 
 private:
+	// 전체 맵의 연결관계를 불러서 m_mapLevel 에 저장해두는 함수
+	HRESULT Load_Level_Data();
+	// 현재 스테이지의 디자인을 불러오는 함수
 	HRESULT Load_Stage_Data(int iType);
+
+	void	Create_GameObject(int iObjType, int iIndex, float x, float y, float z);
 
 private:
 	HRESULT			Ready_Layer_Environment(const _tchar* pLayerTag);
 	HRESULT			Ready_Layer_GameLogic(const _tchar* pLayerTag);
+	HRESULT			Ready_Layer_GameObject(const _tchar* pLayerTag);
 	HRESULT			Ready_Layer_UI(const _tchar* pLayerTag);
 	HRESULT			Ready_LightInfo();
 
@@ -29,5 +43,14 @@ public:
 private:
 	virtual void Free() override;
 
+private:
+	map<int, string> m_mapLevel;
+	map<int, LoadObj> m_mapLoadObj;
+
+	CLoading* m_pLoading;
+
+	int m_iCurStageKey;
+
+	vector<int> m_vecMonsterCount;
 };
 
