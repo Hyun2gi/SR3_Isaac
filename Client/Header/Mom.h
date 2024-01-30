8bf71ace43ca9cf754b30548295eafdf3b093d15
김보민 <byj060362@gmail.com>
2024-01-30 11:59:33 +0900

    [보민] 0130_11h:59m, 풀 위한 커밋

--- Client/Header/Mom.h
+++ Client/Header/Mom.h
@@ -10,28 +10,23 @@ private:
 	explicit CMom(const CMom& rhs);
 	virtual ~CMom();
 
-	enum MOMSTATE{ MOM_IDLE, MOM_ATTACK, MOM_UP, MOM_WAIT, MOM_END }; // 필요 없을 거 같음
+	enum MOMSTATE{}; // 필요 없을 거 같음
 
 public:
 	virtual HRESULT Ready_GameObject()						 override;
 	virtual _int	Update_GameObject(const _float& fTimeDelta) override;
 	virtual void	LateUpdate_GameObject()					 override;
 	virtual void	Render_GameObject()						 override;
-	
+
 private:
 	virtual HRESULT		Add_Component()						override;
 	virtual void		Motion_Change()						override;
 
-	void				Attack(const _float& fTimeDelta);
-	void				Scale_Change();
-
 private:
-	_bool				m_bReduce;
+	// 변수
 	_int				m_iRandNum;
 	_int				m_iPicNum;
 	_float				m_fFrameSpeed = 1.f;
-
-	MOMSTATE			m_eState;
 
 public:
 	static CMom*	Create(LPDIRECT3DDEVICE9 pGraphicDev);
