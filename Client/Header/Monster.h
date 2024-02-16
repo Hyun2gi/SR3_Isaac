c7160361461ef4d9ac1b49c23280727ba98a4042
김보민 <byj060362@gmail.com>
2024-02-16 12:54:18 +0900

    [보민] 0216_12h:54m, Devil 오타 수정

--- Client/Header/Monster.h
+++ Client/Header/Monster.h
@@ -25,6 +25,7 @@ public:
 	_bool			Get_Dead() { return m_bDead; }
 	void			Set_Dead() { m_bDead = true; }
 	_int			Get_HP() { return m_iHp; }
+	_bool			Get_Hit() { return m_bHit; }
 	void			Hit() { m_bHit = true; }
 	void			Hit_False() { m_bHit = false; }
 	_bool			Get_IsBoss() { return m_bBoss; }
